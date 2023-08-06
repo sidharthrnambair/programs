5ca809fc6fed7f58004820da4dd4e8afba3d6d14
sidharthrnambair <sidharthrnambiar11@gmail.com>
2023-08-07 02:25:14 +0530

    gcd added

---
+++ gcd.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{   int m,n;
+
+    cout<<"enter the two numbers"<<endl;
+    cin>>m>>n;
+    while(m!=n)
+    {
+        if(m>n)
+        m=m-n;
+        else if(n>m)
+        n=n-m;
+    }
+    cout<<m;
+    return 0;
+}
